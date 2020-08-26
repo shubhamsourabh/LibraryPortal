423cb636b4baa6e6faf4ee9505e128d88e564e32
Shubham Sourabh <shubhamsourabh@gmail.com>
2020-08-26 20:09:52 +0530

    Changes to Backend/app.js and package.json

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const userRoutes = require("./routes/users");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/users');
 //const bookRoutes = require("./routes/books");
 //const issueRoutes = require("./routes/issue");
-const cors = require("cors");
+const cors = require('cors');
 
 mongoose.connect(
-  "mongodb://localhost/Library",
+  'mongodb+srv://shubham:Lucky@123@cluster0-unn4z.mongodb.net/Library_mangement_system?retryWrites=true&w=majority',
   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
   (err) => {
     if (!err) {
-      console.log("Database Connected...");
+      console.log('Database Connected...');
     } else {
       console.log(err);
     }
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/users", userRoutes);
+app.use('/users', userRoutes);
 //app.use()
 
 module.exports = app;
